f216c8846741739a69958858cfd23408deee7506
levi <levistewart1123@gmail.com>
2026-08-10 20:47:24 -0400

    Made Intake and Tray classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Intake.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import static com.pedropathing.ivy.commands.Commands.instant;
+import static com.pedropathing.ivy.groups.Groups.parallel;
+
+import com.pedropathing.ivy.Command;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+public class Intake {
+    private MotorEx rollerMotor;
+    private MotorEx conveyorMotor;
+    /**
+     * if our motors are drawing above their current limit, their power is multiplied by this number
+     */
+    private final double overCurrentSpeedMultiplier = 0.1;
+    private double rollerMult = 1;
+    private double conveyorMult = 1;
+    private boolean rollerOn = false;
+    private boolean conveyorOn = false;
+    private boolean reversed = false;
+
+    public void initialize(HardwareMap hwMap){
+        rollerMotor = new MotorEx(hwMap, "Intake rollerMotor", Motor.GoBILDA.RPM_1150);
+        conveyorMotor = new MotorEx(hwMap, "Intake conveyorMotor", Motor.GoBILDA.RPM_1150);
+        rollerMotor.setCachingTolerance(0.01);
+        conveyorMotor.setCachingTolerance(0.01);
+        rollerMotor.setCurrentAlert(100, CurrentUnit.AMPS); //TODO tune this to a reasonable value
+        conveyorMotor.setCurrentAlert(100, CurrentUnit.AMPS);
+    }
+
+    public Command startRoller = instant(() -> {
+        rollerOn = true;
+    })
+            .requiring(this);
+
+    public Command stopRoller = instant(() -> {
+        rollerOn = false;
+    })
+            .requiring(this);
+
+    public Command startConveyor = instant(() -> {
+        conveyorOn = true;
+    })
+            .requiring(this);
+
+    public Command stopConveyor = instant(() -> {
+        conveyorOn = false;
+    })
+            .requiring(this);
+
+    public Command startAll = parallel(
+            startRoller,
+            startConveyor
+    )
+            .requiring(this);
+
+    public Command stopAll = parallel(
+            stopRoller,
+            stopConveyor
+    )
+            .requiring(this);
+
+    public Command toggleReversed = instant(() -> reversed = !reversed)
+            .requiring(this);
+
+    public void update(){
+        if (conveyorMotor.isOverCurrent()){
+            conveyorMult = overCurrentSpeedMultiplier;
+        } else {
+            conveyorMult = 1;
+        }
+        if (rollerMotor.isOverCurrent()){
+            rollerMult = overCurrentSpeedMultiplier;
+        } else {
+            rollerMult = 1;
+        }
+        if (reversed){
+            conveyorMult *= -1;
+            rollerMult *= -1;
+        }
+
+        //TODO double check if not checking if the new power is different increases loop times
+        if (conveyorOn){
+            conveyorMotor.set(1 * conveyorMult);
+        }
+        if (rollerOn){
+            rollerMotor.set(1 * rollerMult);
+        }
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Tray.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import static com.pedropathing.ivy.commands.Commands.instant;
+
+import com.pedropathing.ivy.Command;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
+import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
+
+public class Tray {
+    private MotorGroup slides;
+
+    private final int upPosition = 1000; //in encoder ticks TODO tune this
+    private final int downPosition = 0;
+
+    public boolean isUp = false;
+    public boolean isDown = true;
+    private boolean targetIsUpPosition = false;
+
+    public void initialize(HardwareMap hwMap){
+        MotorEx leftSlide = new MotorEx(hwMap, "Tray leftSlide");//TODO add gobilda type/(cpr and rpm)
+        MotorEx rightSlide = new MotorEx(hwMap, "Tray rightSlide");
+        slides = new MotorGroup(leftSlide, rightSlide);
+        slides.setRunMode(Motor.RunMode.PositionControl);
+        slides.setPositionCoefficient(0.05); //TODO tune this (kP)
+        slides.setPositionTolerance(2); //TODO tune this
+        slides.stopAndResetEncoder();
+    }
+
+    public Command setUp = instant(() -> {
+        slides.setTargetPosition(upPosition);
+        targetIsUpPosition = true;
+    });
+
+    public Command setDown = instant(() -> {
+        slides.setTargetPosition(downPosition);
+        targetIsUpPosition = false;
+    });
+
+
+
+    public void update(){
+        if (!slides.atTargetPosition()) {
+            slides.set(0.75); //todo tune this
+        } else {
+            slides.stopMotor();
+        }
+        if (slides.atTargetPosition()){
+            if (targetIsUpPosition) {
+                isUp = true;
+                isDown = false;
+            } else {
+                isDown = true;
+                isUp = false;
+            }
+        } else {
+            isUp = false;
+            isDown = false;
+        }
+    }
+}
