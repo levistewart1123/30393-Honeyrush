8aa326b5274b550ecf6b140cb5398739d38624ec
sectr <sectrftc@southeasternctrobotics.org>
2026-08-20 20:14:55 -0400

    drive bump detection made but not working super well in testing

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/RobotTests.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/RobotTests.java
@@ -35,17 +35,17 @@ public class RobotTests {
         sensorStates.put(Drivetrain.Side.BACK,  true);
         sensorStates.put(Drivetrain.Side.LEFT,  false);
 
-        if (hasUnexpectedPressedSensor(sensorStates)){
+        if (hasUnexpectedPressedSensor(sensorStates, new Pose())){
             butterDown = true;
         } else {
             butterDown = false;
         }
         assertFalse(butterDown);
-        assertFalse(hasUnexpectedPressedSensor(sensorStates));
+        assertFalse(hasUnexpectedPressedSensor(sensorStates, new Pose()));
     }
 
-    public boolean hasUnexpectedPressedSensor(Map<Drivetrain.Side, Boolean> sensorStates) {
-        Set<Drivetrain.Side> expectedTouched = getExpectedTouchedSides();
+    public boolean hasUnexpectedPressedSensor(Map<Drivetrain.Side, Boolean> sensorStates, Pose botPose) {
+        Set<Drivetrain.Side> expectedTouched = getExpectedTouchedSides(botPose);
 
         for (Map.Entry<Drivetrain.Side, Boolean> entry : sensorStates.entrySet()) {
             Drivetrain.Side side = entry.getKey();
@@ -58,9 +58,9 @@ public class RobotTests {
         return false;
     }
 
-    public Set<Drivetrain.Side> getExpectedTouchedSides() {
+    public Set<Drivetrain.Side> getExpectedTouchedSides(Pose botPose) {
         Set<Drivetrain.Side> touchedSides = new HashSet<>();
-        Pose[] corners = getRobotCorners();
+        Pose[] corners = getRobotCorners(botPose);
         double heading = new Pose(1, 1.5, 0).getHeading();
 
         double frontNx = Math.cos(heading),  frontNy = Math.sin(heading);
@@ -91,8 +91,7 @@ public class RobotTests {
         return touchedSides;
     }
 
-    public Pose[] getRobotCorners() {
-        Pose robotPose = new Pose(1, 1.5, 0);
+    public Pose[] getRobotCorners(Pose robotPose) {
         double half = 9.0; // half of 18 inches
         double x = robotPose.getX();
         double y = robotPose.getY();
@@ -151,20 +150,65 @@ public class RobotTests {
         return local;
     }
 
+    @Test
     public void drive(){
         double forward = 1, strafe = 0,  turn = 0;
-        Pose botPose = new Pose(72, 40, 45);
+        Pose botPose = new Pose(72, 39, 45);
         if (false) {
             forward *= 0.2;
             strafe *= 0.2;
             turn *= 0.2;
         }
+        Pose[] corners = getRobotCorners(botPose);
+        if (isCloseToBump(botPose)) {
+            Vector movement = getFieldRelativeMovement(forward, strafe, botPose.getHeading());
+            double x = movement.getXComponent();
+            double y = movement.getYComponent();
 
-
+            if ((getClosestCorner(botPose).getX() > (BUMP_MIN_X - MARGIN)) && (getClosestCorner(botPose).getX() < BUMP_MIN_X)){
+                x = -0.5;
+            } else if (getClosestCorner(botPose).getX() < (BUMP_MAX_X + MARGIN) && getClosestCorner(botPose).getX() > BUMP_MAX_X) {
+                x = 0.5;
+            }
+            Pose corner = getClosestCorner(botPose);
+            boolean below = getClosestCorner(botPose).getY() > (BUMP_MIN_Y - MARGIN) && getClosestCorner(botPose).getY() < BUMP_MIN_Y;
+            if (getClosestCorner(botPose).getY() > (BUMP_MIN_Y - MARGIN) && getClosestCorner(botPose).getY() < BUMP_MIN_Y){
+                y = -0.5;
+            } else if (getClosestCorner(botPose).getY() < (BUMP_MAX_Y + MARGIN) && getClosestCorner(botPose).getY() > BUMP_MAX_Y) {
+                y = -0.5;
+            }
+            movement.setOrthogonalComponents(x, y);
+            forward = getRobotRelativeMovement(movement, turn).getXComponent();
+            strafe = getRobotRelativeMovement(movement, turn).getYComponent();
+        }
     }
 
-//    public boolean isCloseToBump(){
-//
-//    }
+    private final double BUMP_MIN_X = 47.75, BUMP_MIN_Y = 53, BUMP_MAX_X = 96.25, BUMP_MAX_Y = 91, MARGIN = 2.5;
 
+    public boolean isCloseToBump(Pose botPose){
+        for (Pose corner : getRobotCorners(botPose)) {
+            boolean withinX = (corner.getX() > (BUMP_MIN_X - MARGIN) && corner.getX() < (BUMP_MAX_X + MARGIN));
+            boolean withinY = (corner.getY() > (BUMP_MIN_Y - MARGIN) && corner.getY() < (BUMP_MAX_Y + MARGIN));
+            if (withinX && withinY) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public Pose getClosestCorner(Pose botPose){
+        for (Pose corner : getRobotCorners(botPose)) {
+            boolean withinX = (botPose.getX() > BUMP_MIN_X - MARGIN && botPose.getX() < BUMP_MAX_X + MARGIN);
+            boolean withinY = (botPose.getY() > BUMP_MIN_Y - MARGIN && botPose.getY() < BUMP_MAX_Y + MARGIN);
+            return corner;
+        }
+        return new Pose();
+    }
+    /*
+    y>0\left\{45.75\ <x<\ 98.25\right\}
+    x>0\left\{51\ <y<\ 93\right\}
+    \operatorname{polygon}\left(\left(47.75,\ 53\right),\left(47.75,\ 91\right),\left(96.25,\ 91\right),\left(96.25,53\right)\right)
+    P\ =\ \left(72,39+\left(9\cdot\sqrt{2}\right)\right)
+     */
 }
